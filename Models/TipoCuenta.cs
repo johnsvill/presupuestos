7526acd07a3e654c97e279c5c25936a19161692a
Jonathan Villeda <johnsvill@hotmail.com>
2022-01-31 13:31:09 -0600

    Empezando proyecto

---
+++ Models/TipoCuenta.cs
@@ -0,0 +1,32 @@
+﻿using Presupuestos.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace Presupuestos.Models
+{
+    public class TipoCuenta : IValidatableObject
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [PrimeraLetraMayus]
+        public string Nombre { get; set; }
+
+        public int UsuarioId { get; set; }  
+
+        public int Orden { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Nombre != null && Nombre.Length > 0)
+            {
+                var primeraLetra = Nombre[0].ToString();
+
+                if(primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayúscula.",
+                        new[] { nameof(Nombre) });                    
+                }            
+            }
+        }
+    }
+}
